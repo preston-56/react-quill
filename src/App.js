c9de98f35bc728f59c2371ca35f62ce0b9cb9fa5
Preston <prestonosoro56@gmail.com>
2023-06-21 02:53:47 +0300

    additional quill features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import ReactQuill from "react-quill";
+import React, { useState, useEffect } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import ReactDOM from "react-dom";
 import "react-quill/dist/quill.snow.css";
-
+import { Picker } from "emoji-mart";
 import "./App.css";
 
 const modules = {
@@ -17,21 +18,68 @@ const modules = {
       { indent: "+1" },
     ],
     ["link", "image", "video"],
+    [{ align: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ color: [] }, { background: [] }],
+    ["clean"],
+    ["emoji"],
   ],
+  clipboard: {
+    matchVisual: false,
+  },
 };
+
+// Custom emoji module for Quill
+const EmojiModule = () => {
+  const emojiButton = document.querySelector(".ql-emoji");
+  if (!emojiButton) return;
+
+  emojiButton.addEventListener("click", () => {
+    const picker = document.createElement("div");
+    picker.style.position = "absolute";
+    picker.style.zIndex = "10000";
+    document.body.appendChild(picker);
+
+    const onEmojiSelect = (emoji) => {
+      const range = this.quill.getSelection();
+      this.quill.insertText(range.index, emoji.native);
+      this.quill.setSelection(range.index + emoji.native.length);
+      picker.remove();
+    };
+
+    ReactDOM.render(<Picker onSelect={onEmojiSelect} />, picker);
+  });
+};
+
+Quill.register("modules/emoji", EmojiModule);
 
 function App() {
   const [value, setValue] = useState("");
 
+  useEffect(() => {
+    const quill = quillRef.current.getEditor();
+
+    // Add the custom emoji module to the toolbar
+    const toolbar = quill.getModule("toolbar");
+    toolbar.addHandler("emoji", () => {
+      quill.focus();
+      quill.blur(); // Fixes an issue where the emoji picker doesn't appear on the first click
+      quill.focus();
+    });
+  }, []);
+
   const handleChange = (content) => {
     setValue(content);
   };
+
+  const quillRef = React.useRef(null);
 
   return (
     <div className="container">
       <div className="row">
         <div className="editor">
           <ReactQuill
+            ref={quillRef}
             theme="snow"
             value={value}
             onChange={handleChange}
@@ -41,9 +89,7 @@ function App() {
         </div>
         <div className="preview">
           {value.length === 0 ? "Preview" : null}
-          <div
-            dangerouslySetInnerHTML={{ __html: value }}
-          ></div>
+          <div dangerouslySetInnerHTML={{ __html: value }}></div>
         </div>
       </div>
     </div>
